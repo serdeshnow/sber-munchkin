267e7b17a284a03f7dcea6b73ed749c44e390c0b
Serdeshnow <vserdes1977@mail.ru>
2025-05-12 21:14:26 +0300

    [styles](project): Button & Header

--- frontend/src/shared/ui/Button.tsx
+++ frontend/src/shared/ui/Button.tsx
@@ -2,44 +2,50 @@ import React from 'react';
 import cn from 'classnames';
 import { Button as HeroButton, ButtonProps as HeroButtonProps } from '@heroui/react';
 
-type ButtonColor = 'primary' | 'secondary' | 'gray' | 'error' | 'logout';
+export type ButtonColor = 'primary' | 'secondary' | 'gray' | 'error' | 'logout' | 'disabled';
 
 const colorMap: Record<ButtonColor, { bg: string; text: string }> = {
-  primary: { bg: 'bg-brown-500', text: 'text-light-gray-500' },
-  secondary: { bg: 'bg-secondary-500', text: 'text-light-gray-500' },
-  gray: { bg: 'bg-secondary-600', text: 'text-light-gray-500' },
-  error: { bg: 'bg-error-300', text: 'text-error-600' },
-  logout: { bg: 'bg-none', text: 'text-error-500' },
+  primary: {bg: 'bg-brown-500', text: 'text-light-gray-500'},
+  secondary: {bg: 'bg-secondary-500', text: 'text-light-gray-500'},
+  gray: {bg: 'bg-secondary-600', text: 'text-light-gray-500'},
+  error: {bg: 'bg-error-300', text: 'text-error-600'},
+  logout: {bg: 'bg-transparent', text: 'text-error-500'},
+  disabled: {bg: 'bg-dark-gray-500', text: 'text-light-gray-500'},
 };
 
 export interface ButtonProps extends Omit<HeroButtonProps, 'color'> {
   color?: ButtonColor;
-  startContentIcon?: React.ReactNode;
+  startIcon?: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  variant = 'solid',
-  size = 'md',
-  radius = 'md',
-  color = 'primary',
-  startContentIcon,
-  className = '',
-  children,
-  ...props
-}) => {
-  const selected = colorMap[color];
+export const Button: React.FC<ButtonProps> = (
+  {
+    variant = 'solid',
+    size = 'md',
+    radius = 'md',
+    color = 'primary',
+    startIcon,
+    disabled = false,
+    className,
+    children,
+    ...rest
+  }) => {
+  const key = disabled ? 'disabled' : color;
+  const {bg, text} = colorMap[key];
 
   return (
     <HeroButton
-      {...props}
+      {...rest}
       variant={variant}
       size={size}
       radius={radius}
-      startContent={startContentIcon}
+      startContent={startIcon}
+      disabled={disabled}
       className={cn(
-        selected.bg,
-        selected.text,
-        `text-base font-bold py-3`,
+        bg,
+        text,
+        'text-base font-bold py-3',
+        disabled && 'cursor-not-allowed opacity-70',
         className,
       )}
     >
--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -4,7 +4,7 @@ import { useGame } from '@app/providers/GameProvider.tsx';
 
 
 export const Header: React.FC = () => {
-  const { openReset, openAdd } = useGame();
+  const {users, openReset, openAdd} = useGame();
 
   return (
     <header className="flex justify-between bg-dark-gray-500 px-10 py-4">
@@ -12,15 +12,16 @@ export const Header: React.FC = () => {
       <div className="flex gap-3 w-1/2">
 
         <Button
-          className="px-6 text-2xl"
+          className="px-6 text-2xl w-full"
+          disabled={users.length >= 6}
           onClick={openAdd}>
           <Icon type="plus"/> Добавить манчкина
         </Button>
         <Button
-          className="px-6 text-2xl"
+          className="px-6 text-2xl w-full"
           onClick={openReset}
-          startContentIcon={<Icon type="restart"/>}
         >
+          <Icon type="restart"/>
           Рестарт
         </Button>
       </div>
