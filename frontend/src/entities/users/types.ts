47d3553a55dea3aa46aabcfcacfebffa4337135b
PdrsDv3 <denis.kalinkin.05@gmail.com>
2025-05-12 19:44:50 +0300

    accuratno merge conflict

--- frontend/src/entities/users/types.ts
+++ frontend/src/entities/users/types.ts
@@ -1,10 +1,11 @@
+export interface UserRecord {
+  id: number;
+  session: PlayerSession[];
+}
+
 // src/entities/users/types.ts
 export interface PlayerSession {
   username: string;
   level: number;
   power: number;
-}
-export interface UserRecord {
-  id: number;
-  session: PlayerSession;
 }
