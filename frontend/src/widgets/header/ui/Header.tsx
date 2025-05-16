241854a9a255dd3928f7501b4e47c1aeaf0156f3
Serdeshnow <vserdes1977@mail.ru>
2025-05-16 18:30:39 +0300

    [styles](project): header

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -9,25 +9,32 @@ export const Header: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex justify-between bg-dark-gray-500 px-10 py-4">
+    <header className="flex justify-between bg-dark-gray-500 px-10 py-4 gap-50">
       <Title
         onClick={() => navigate('/')}
       >Манчкины</Title>
       {
         location.pathname === '/game' && (
-          <div className="flex gap-3 w-1/2">
+          <div className="flex gap-3 w-full justify-end">
             <Button
-              className="px-6 text-2xl w-full"
+              className="px-6 text-2xl w-auto"
               disabled={users.length >= 6}
               onClick={openAdd}>
               <Icon type="plus"/> Добавить манчкина
             </Button>
             <Button
-              className="px-6 text-2xl w-full"
+              className="px-6 text-2xl w-auto"
               onClick={openReset}
             >
               <Icon type="restart"/>
               Рестарт
+            </Button>
+            <Button
+              onClick={() => navigate('/support')}
+              className="px-6 text-2xl w-auto"
+              startContent={<Icon type="question" />}
+            >
+              Справка
             </Button>
           </div>
         )
